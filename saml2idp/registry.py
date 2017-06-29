1dd28ea2608d8f97c4d324d2617d28779178353a
Thys <thys@unomena.com>
2017-06-29 15:44:15 +0200

    Removed the error raised 'None of the processors in SAML2IDP_REMOTES could handle this request' and added debug log

--- saml2idp/registry.py
+++ saml2idp/registry.py
@@ -1,17 +1,18 @@
 """
 Registers and loads Processor classes from settings.
 """
-# Python imports
+
 import logging
-# Django imports
+
 from django.utils.importlib import import_module
 from django.core.exceptions import ImproperlyConfigured
-# Local imports
+
 import exceptions
 import saml2idp_metadata
 
-# Setup logging
+
 logger = logging.getLogger(__name__)
+
 
 def get_processor(dottedpath):
     """
@@ -23,19 +24,30 @@ def get_processor(dottedpath):
     try:
         dot = dottedpath.rindex('.')
     except ValueError:
-        raise ImproperlyConfigured('%s isn\'t a processors module' % dottedpath)
-    sp_module, sp_classname = dottedpath[:dot], dottedpath[dot+1:]
+        raise ImproperlyConfigured(
+            '%s isn\'t a processors module' % dottedpath
+        )
+
+    sp_module, sp_classname = dottedpath[:dot], dottedpath[dot + 1:]
     try:
         mod = import_module(sp_module)
     except ImportError, e:
-        raise ImproperlyConfigured('Error importing processors %s: "%s"' % (sp_module, e))
+        raise ImproperlyConfigured(
+            'Error importing processors %s: "%s"' % (sp_module, e)
+        )
+
     try:
         sp_class = getattr(mod, sp_classname)
     except AttributeError:
-        raise ImproperlyConfigured('processors module "%s" does not define a "%s" class' % (sp_module, sp_classname))
+        raise ImproperlyConfigured(
+            'processors module "%s" does not define a "%s" class' % (
+                sp_module, sp_classname
+            )
+        )
 
     instance = sp_class()
     return instance
+
 
 def find_processor(request):
     """
@@ -49,4 +61,7 @@ def find_processor(request):
         except exceptions.CannotHandleAssertion, e:
             # Log these, but keep looking.
             logger.debug('%s %s' % (proc, e))
-    raise exceptions.CannotHandleAssertion('None of the processors in SAML2IDP_REMOTES could handle this request.')
+
+    logger.debug(
+        'None of the processors in SAML2IDP_REMOTES could handle this request'
+    )
