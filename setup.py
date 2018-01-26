918aeacc05df1a44020e97971286ce72f32a7f68
Thys Jamneck <thys@unomena.com>
2018-01-26 11:59:19 +0200

    Upped the version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='saml2idp',
-    version='0.2.0',
+    version='0.3.0',
     description='SAML 2.0 Django Application',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read(),
     author='Unomena',
