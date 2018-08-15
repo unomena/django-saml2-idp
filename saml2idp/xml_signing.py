558405c273bf6caff1ef62dd1f1f7565501d5ebe
Thys Jamneck <thys@unomena.com>
2018-08-15 17:44:01 +0200

    Fixed issue: Can't convert 'bytes' object to str implicitly

--- saml2idp/xml_signing.py
+++ saml2idp/xml_signing.py
@@ -12,6 +12,7 @@ from . import saml2idp_metadata
 from .codex import nice64
 from .xml_templates import SIGNED_INFO, SIGNATURE
 
+
 def load_cert_data(certificate_file):
     """
     Returns the certificate data out of the certificate_file.
@@ -19,6 +20,7 @@ def load_cert_data(certificate_file):
     certificate = M2Crypto.X509.load_cert(certificate_file)
     cert_data = ''.join(certificate.as_pem().split('\n')[1:-2])
     return cert_data
+
 
 def get_signature_xml(subject, reference_uri):
     """
@@ -36,13 +38,13 @@ def get_signature_xml(subject, reference_uri):
     subject_hash = hashlib.sha1()
     subject_hash.update(subject.encode('utf-8'))
     subject_digest = nice64(subject_hash.digest())
-    logging.debug('Subject digest: ' + subject_digest)
+    # logging.debug('Subject digest: ' + subject_digest)
 
     # Create signed_info.
     signed_info = string.Template(SIGNED_INFO).substitute({
         'REFERENCE_URI': reference_uri,
         'SUBJECT_DIGEST': subject_digest,
-        })
+    })
     logging.debug('SignedInfo XML: ' + signed_info)
 
     # RSA-sign the signed_info.
@@ -50,17 +52,20 @@ def get_signature_xml(subject, reference_uri):
     private_key.sign_init()
     private_key.sign_update(signed_info)
     rsa_signature = nice64(private_key.sign_final())
-    logging.debug('RSA Signature: ' + rsa_signature)
+    # logging.debug('RSA Signature: ' + rsa_signature)
 
     # Load the certificate.
     cert_data = load_cert_data(certificate_file)
 
     # Put the signed_info and rsa_signature into the XML signature.
-    signed_info_short = signed_info.replace(' xmlns:ds="http://www.w3.org/2000/09/xmldsig#"', '')
+    signed_info_short = signed_info.replace(
+        ' xmlns:ds="http://www.w3.org/2000/09/xmldsig#"',
+        ''
+    )
     signature_xml = string.Template(SIGNATURE).substitute({
         'RSA_SIGNATURE': rsa_signature,
         'SIGNED_INFO': signed_info_short,
         'CERTIFICATE': cert_data,
-        })
+    })
     logging.debug('Signature XML: ' + signature_xml)
     return signature_xml
